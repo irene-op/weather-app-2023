42f388d6fc2b4f562cc78ecd0062e83a4661adc9
Irene Ortega <72819188+irene-op@users.noreply.github.com>
2023-08-17 17:31:00 +0200

    Search engine added and working. Currently landing/loading on fixed city

--- src/app.js
+++ src/app.js
@@ -1,31 +1,3 @@
-
-
-//Change city h1
-// function searchCity(event) {
-//     event.preventDefault();
-//     let city = document.querySelector("#city");
-//     let inputCity = document.querySelector("#input-city");
-//     city.innerHTML = `${inputCity.value}`;
-// }
-
-// let searchForm = document.querySelector("#search-form");
-// searchForm.addEventListener("submit", searchCity);
-
-
-// get weather data with Open Weather API
-function getWeather(response) {
-    let currentTemperature = document.querySelector("#current-temperature")
-    let h1 = document.querySelector("h1");
-    let description = document.querySelector("#description");
-    let humidity = document.querySelector("#humidity");
-    let windSpeed = document.querySelector("#wind-speed");
-    h1.innerHTML = response.data.name;
-    currentTemperature.innerHTML = `${Math.round(response.data.main.temp)}`;
-    description.innerHTML = response.data.weather[0].description;
-    humidity.innerHTML = response.data.main.humidity;
-    windSpeed.innerHTML = Math.round(response.data.wind.speed);
-}
-
 // get and format date data with Open Weather API
 function getDate(response) {
     let currentTime = document.querySelector("#current-time");
@@ -61,10 +33,50 @@ function getDate(response) {
 
     currentDay.innerHTML = `${weekDays[day]}`;
     currentDate.innerHTML = `${dayNum}/${month}/${year}`;
-    currentTime.innerHTML = `Last updated: ${hours}:${minutes}`;
+    currentTime.innerHTML = `Last updated: ${hours}:${minutes}`; //deal with this because it's not current time but last updated. Make it make sense
 }
 
-let city = "New York";
+// get weather data with Open Weather API
+function getWeather(response) {
+    let currentTemperature = document.querySelector("#current-temperature")
+    let h1 = document.querySelector("h1");
+    let description = document.querySelector("#description");
+    let humidity = document.querySelector("#humidity");
+    let windSpeed = document.querySelector("#wind-speed");
+    let mainIcon = document.querySelector("#main-icon");
+
+    mainIcon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    h1.innerHTML = response.data.name;
+    currentTemperature.innerHTML = `${Math.round(response.data.main.temp)}`;
+    description.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+    console.log(response.data);
+    console.log(response.data.sys.country);
+    console.log(response.data.weather[0].icon);
+}
+
+// Search city
+function searchCity(city) {
+    let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getWeather);
+    axios.get(apiUrl).then(getDate);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let inputCity = document.querySelector("#input-city");
+    searchCity(inputCity.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+//Show on load (ultimately, I want the page to load on 'current location')
+let city = "Aarhus";
 let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
